6e4d54b4c37aad5f48491fa18fbcbd57d88dcb10
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2022-07-02 20:29:47 +0200

    Update hw_sim_fwk_clock.vhd
    use of fifo (named pipe) which shall be faster than file-io

--- vhdl/hw_sim_fwk/hw_sim_fwk_clock.vhd
+++ vhdl/hw_sim_fwk/hw_sim_fwk_clock.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 use STD.textio.all;
 use ieee.std_logic_textio.all;
 library hw_sim_fwk;
-use hw_sim_fwk.hw_sim_fwk_common.all;
+--mm use hw_sim_fwk.hw_sim_fwk_common.all;
 
 -- Note: the followig metacomment is not part of the VHDL synthesis standard (IEEE P1076.6) so behavior is tool dependent.
 -- synthesis translate_off
@@ -15,11 +15,8 @@ use hw_sim_fwk.hw_sim_fwk_common.all;
 entity hw_sim_fwk_clock is
     generic(
         FILE_PATH                 : string  := "/tmp/";
-        CLOCK_FILE_NAME           : string  := "/tmp/clock_high"; -- FILE_PATH & "clock_high" -- cannot use concatenation here!
+        CLOCK_FILE_NAME           : string  := "/tmp/hw_sim_fwk/clock"; -- FILE_PATH & "clock_high" -- cannot use concatenation here!
         CLOCK_PERIOD              : time    := 20 ns;
-        -- Note: 50000000 works fine with CLOCK_PERIOD = 20ns and CLOCK_PERIOD_EXTERNAL = 1sec
-        --       according to the formula CLOCK_PERIOD_EXTERNAL/CLOCK_PERIOD both in units of ns
-        MAX_ELAPSED_CLOCK_PERIODS : integer := 50000000;
         PROTOCOL_CLOCK            : boolean := false -- to report details
     );
     port(
@@ -32,17 +29,29 @@ begin
     -- polling process
     -- ###############
     proc_update_clock : process
+        file     i_file      : text;
+        variable open_status : file_open_status;
+        variable i_line      : line;
+        -- NOTE: the variable "stimulus" could mean clock_level or edge_transition, but it does not matter
+        --       because to keep things consistent we check in addition against the variable "pos_edge".
+        variable stimulus   : std_logic;
         variable pos_edge            : boolean := false;
-        variable elapsed_clk_periods : integer := 0;
-        variable elapsed_checks      : integer := 0;
         variable initialized         : boolean := false;
     begin
-        elapsed_clk_periods := elapsed_clk_periods + 1;
         -- initialization?
         if initialized = false then
             initialized := true;
-            -- initialize clock signal depending on file existence
-            if (file_exists(CLOCK_FILE_NAME) = true) then
+            -- open stimulus file
+            loop
+                file_open(open_status, i_file, CLOCK_FILE_NAME,  read_mode);
+                exit when open_status = open_ok;
+            end loop;
+            -- "block" until new line is sent (no CPU consumption!)
+            -------------------------------------------------------
+            readline(i_file, i_line);
+            read(i_line, stimulus);
+            -- initialize hw_clock with external clock signal
+            if stimulus = '1' then
                 hw_clock <= '1';
                 pos_edge := true;
                 -- if (PROTOCOL_CLOCK = true) then
@@ -55,43 +64,45 @@ begin
                 report "hw_clock initialized to 0";
                 -- end if;
             end if;
-        -- check for rising-edge, now or later?
-        -- NOTE: although this avoids checking permanently (and probably blocking) the input file
-        --       it does not avoid looping "without pause" and thus consuming 100% of a CPU-Core!
-        -- TODO: find out if it is possible somehow to pause/sleep between external clock cycles
-        --       but "without resuming simulation".
-        elsif (elapsed_clk_periods = MAX_ELAPSED_CLOCK_PERIODS) then
-            elapsed_clk_periods := 0;
-            elapsed_checks      := elapsed_checks + 1;
-            if (PROTOCOL_CLOCK = true) then
-                report "checking clk_rising_edge..";
-            end if;
-            -- file clock H exists:
-            if (file_exists(CLOCK_FILE_NAME) = true) then
-                -- otherwise we generate additional clock cycles which do not correspond with external GUI simulation
-                -- rising edge?
-                if pos_edge = false then
-                    if (PROTOCOL_CLOCK = true) then
-                        report "Nr of checks until clk_rising_ege = " & integer'image(elapsed_checks);
+        -- check external clock signal
+        else
+            -- with this loop we avoid re-checking the variable "initialized" unnecessarily            
+            loop
+                -- "block" until a new line is sent in fifo (named pipe)
+                -------------------------------------------------------
+                readline(i_file, i_line);
+                read(i_line, stimulus);
+                if (PROTOCOL_CLOCK = true) then
+                    report "checking external clock..";
+                end if;
+                -- evaluate external clock signal
+                if stimulus = '1' then
+                
+                    -- as an error, the external clock signal may repeat the same edge transition
+                    -- therefore we always check current state of pos_edge
+                    -- rising edge?
+                    if pos_edge = false then
+                        pos_edge       := true;
+                        hw_clock       <= '1';
+                        wait for CLOCK_PERIOD / 2;
                     end if;
-                    elapsed_checks := 0;
-                    pos_edge       := true;
-                    hw_clock       <= '1';
-                    wait for CLOCK_PERIOD / 2;
+                -- stimulus /= '1':
+                else
+                    -- falling edge?
+                    if pos_edge = true then
+                        pos_edge := false;
+                        hw_clock <= '0';
+                        wait for CLOCK_PERIOD / 2;
+                    end if;
                 end if;
-            -- file clock H does not exist:
-            else
-                -- falling edge?
-                if pos_edge = true then
-                    pos_edge := false;
-                    hw_clock <= '0';
-                    wait for CLOCK_PERIOD / 2;
-                end if;
-            end if;
+            end loop;
+            -- TODO: implement condition (e.g. special value in clock stimulus) to get here..
+            --       and add another flag (?) to get out of the process (?)
+            -- file_close(i_file);
+            -- report "Left process proc_update_clock in hw_sim_fwk_clock"; -- severity note;
         end if;
     end process proc_update_clock;
 end arch;
 
 -- synthesis translate_on
 -- ######################
-
