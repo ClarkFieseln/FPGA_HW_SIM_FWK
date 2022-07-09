44310c34b5b96d9e3340c62ef267a90b063c3150
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2022-07-09 14:18:10 +0200

    Update generate_from_ui.bat
    added argument to have correct reference to .py image files..
    --import-from=dist/icons

--- python/hw_sim_fwk/ui/generate_from_ui.bat
+++ python/hw_sim_fwk/ui/generate_from_ui.bat
@@ -1,5 +1,6 @@
 # sudo python3 -m PyQt6.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
-sudo python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
+# sudo python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
+sudo python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py --import-from=dist/icons
 # python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
 # python -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
 
