696c1b837559fccf5cd24a0ca823d4b784012729
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2022-08-11 13:04:27 +0200

    Update hw_sim_fwk_clock.vhd
    dont use readline() anymore.
    Now we only read 1 byte and avoid transmission and reception of CR+LF

--- vhdl/hw_sim_fwk/hw_sim_fwk_clock.vhd
+++ vhdl/hw_sim_fwk/hw_sim_fwk_clock.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 use STD.textio.all;
 use ieee.std_logic_textio.all;
 library hw_sim_fwk;
---mm use hw_sim_fwk.hw_sim_fwk_common.all;
+
 
 -- Note: the followig metacomment is not part of the VHDL synthesis standard (IEEE P1076.6) so behavior is tool dependent.
 -- synthesis translate_off
@@ -14,10 +14,10 @@ library hw_sim_fwk;
 
 entity hw_sim_fwk_clock is
     generic(
-        FILE_PATH                 : string  := "/tmp/";
-        CLOCK_FILE_NAME           : string  := "/tmp/hw_sim_fwk/clock"; -- FILE_PATH & "clock_high" -- cannot use concatenation here!
+        FIFO_PATH                 : string  := "\\.\pipe\";
+        CLOCK_FILE_NAME           : string  := FIFO_PATH & "clock_high";
         CLOCK_PERIOD              : time    := 20 ns;
-        PROTOCOL_CLOCK            : boolean := false -- to report details
+        PROTOCOL_CLOCK            : boolean := false
     );
     port(
         hw_clock : out std_logic
@@ -26,60 +26,68 @@ end hw_sim_fwk_clock;
 
 architecture arch of hw_sim_fwk_clock is
 begin
-    -- process to read FIFO
-    -- ####################
+    -- clock reading process
+    -- #####################
     proc_update_clock : process
-        file     i_file      : text;
+        type CHARACTER_FILE is file of character;
+        file     i_file      : CHARACTER_FILE;
         variable open_status : file_open_status;
-        variable i_line      : line;
         -- NOTE: the variable "stimulus" could mean clock_level or edge_transition, but it does not matter
         --       because to keep things consistent we check in addition against the variable "pos_edge".
-        variable stimulus   : std_logic;
+        variable stimulus   : character;
         variable pos_edge            : boolean := false;
         variable initialized         : boolean := false;
     begin
         -- initialization?
+        -- ###############
         if initialized = false then
             initialized := true;
-            -- open stimulus file
+            
+            if (PROTOCOL_CLOCK = true) then
+                report "simulated clock waiting to open for read on FIFO.";
+            end if;
+            -- open stimulus FIFO/named-pipe (created in external simulation app - VHDL is NOT able to create named pipes!)
             loop
                 file_open(open_status, i_file, CLOCK_FILE_NAME,  read_mode);
                 exit when open_status = open_ok;
             end loop;
             -- "block" until new line is sent (no CPU consumption!)
             -------------------------------------------------------
-            readline(i_file, i_line);
-            read(i_line, stimulus);
+            read(i_file, stimulus);
             -- initialize hw_clock with external clock signal
             if stimulus = '1' then
                 hw_clock <= '1';
                 pos_edge := true;
-                -- if (PROTOCOL_CLOCK = true) then
-                report "hw_clock initialized to 1";
-                -- end if;
+                if (PROTOCOL_CLOCK = true) then
+                    report "hw_clock initialized to 1";
+                end if;
             else
                 hw_clock <= '0';
                 pos_edge := false;
-                -- if (PROTOCOL_CLOCK = true) then
-                report "hw_clock initialized to 0";
-                -- end if;
+                if (PROTOCOL_CLOCK = true) then
+                    report "hw_clock initialized to 0";
+                end if;
             end if;
+            -- TODO: check if we need this here:
+            wait for CLOCK_PERIOD / 2;
         -- check external clock signal
+        -- ###########################
         else
-            -- with this loop we avoid re-checking the variable "initialized" unnecessarily            
+            -- with this loop we avoid re-checking the variable "initialized" every time      
             loop
-                -- "block" until a new line is sent in fifo (named pipe)
-                -------------------------------------------------------
-                readline(i_file, i_line);
-                read(i_line, stimulus);
+                -- "block" until a new line is sent over the FIFO (named pipe), no CPU consumption!
+                -- WARNING!
+                --         in VIVADO the complete simulation blocks. Processes are not concurrent in this particular case.
+                --         For clock this is not problem because everything is driven by it.
+                ----------------------------------------------------------------------------------------------------------
+                read(i_file, stimulus);
                 if (PROTOCOL_CLOCK = true) then
                     report "checking external clock..";
                 end if;
                 -- evaluate external clock signal
-                if stimulus = '1' then
-                
-                    -- as a result of an error, the external clock signal may repeat the same edge transition
-                    -- therefore we always check current state of pos_edge
+                if stimulus = '1' then               
+                    -- as an error, the external clock signal may repeat/resend the same edge transition
+                    -- therefore, we always check the current state of pos_edge
                     -- rising edge?
                     if pos_edge = false then
                         pos_edge       := true;
