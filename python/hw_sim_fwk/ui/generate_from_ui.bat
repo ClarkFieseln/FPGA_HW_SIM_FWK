c036566cae4cb4abb826c552cd2437f46c9cff5b
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2022-08-11 14:33:46 +0200

    Update generate_from_ui.bat
    update for Windows system

--- python/hw_sim_fwk/ui/generate_from_ui.bat
+++ python/hw_sim_fwk/ui/generate_from_ui.bat
@@ -1,6 +1,5 @@
-# sudo python3 -m PyQt6.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
-# sudo python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
-sudo python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py --import-from=dist.icons
-# python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
-# python -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
+python -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py --import-from=dist.icons
+REM sudo python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py --import-from=dist.icons
+REM python3 -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
+REM python -m PyQt5.uic.pyuic -x mainWindow.ui -o Ui_mainWindow.py
 
